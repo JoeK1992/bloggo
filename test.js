59335d13341cb3fc91e7a0d59ee15384f2143fef
DCrawley94 <duncan.crawley@gmail.com>
2021-04-16 11:45:16 +0100

    husky/eslint test

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-const add = (num1, num2) => {
-  return num1 + num2;
-};
+const add = (num1, num2) => num1 + num2;
 
 add(1, 2);
 
-console.log('yes', 'yes', 'yes');
+console.log("yes", "yes", "yes", "yes", "yes");
