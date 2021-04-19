0bee5ebe456159e70ddf6cf6fa60c7336bc3a798
elena-cav <elenacavallero@outlook.com>
2021-04-19 16:12:52 +0100

    add destination form and image picker - error to solve with cloudinary

---
+++ src/screens/AddDestinationScreen/AddDestinationScreen.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import {
+  View, Text, TextInput, Alert,
+} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import CalendarPicker from 'react-native-calendar-picker';
+import UploadImage from './UploadImage';
+import firebase from '../../firebase/config';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+export default function AddDestinationScreen() {
+  const db = firebase.firestore();
+  const [blogPost, setBlog] = useState('');
+
+  const currentUserUID = firebase.auth().currentUser.uid;
+  const [selectedStartDate, setStartDate] = useState(null);
+  const [selectedEndDate, setEndDate] = useState(null);
+  const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+  const endDate = selectedEndDate ? selectedEndDate.toString() : '';
+  const onDateChange = (date, type) => {
+    if (type === 'END_DATE') {
+      setEndDate(date);
+    } else {
+      setStartDate(date);
+      setEndDate(null);
+    }
+  };
+
+  const handlePress = () => {
+    if (!startDate) {
+      Alert.alert('Start Date field is required.');
+    }
+    if (!endDate) {
+      Alert.alert('End Date field is required.');
+    }
+
+    db.collection('trips').add({
+      user: currentUserUID,
+      trip: 'KNC4mJjToxN1jp8XTYOb',
+      blogPost,
+      startDate,
+      endDate,
+    });
+    setBlog('');
+    setStartDate('');
+    setEndDate('');
+  };
+
+  return (
+    <View>
+      <Text>Add trip:</Text>
+      <Text>Destination Name</Text>
+      <Text>Select the dates of your stay</Text>
+
+      <CalendarPicker
+        startFromMonday
+        allowRangeSelection
+        todayBackgroundColor="#f2e6ff"
+        selectedDayColor="#7300e6"
+        selectedDayTextColor="#FFFFFF"
+        onDateChange={onDateChange}
+        scaleFactor="700"
+      />
+
+      <View>
+        <Text>
+          SELECTED START DATE:
+          {startDate}
+        </Text>
+        <Text>
+          SELECTED END DATE:
+          {endDate}
+        </Text>
+      </View>
+      <TextInput
+        placeholder="Enter your blog post"
+        value={blogPost}
+        onChangeText={(blogPost) => setBlog(blogPost)}
+        autoCapitalize="none"
+      />
+      <UploadImage />
+      <TouchableOpacity onPress={handlePress}>
+        <Text>Sumbit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
