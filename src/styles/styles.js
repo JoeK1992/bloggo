3950de4c22c11a3600f6f1dfd301b5252d300cad
Gary OConnor <garyoconnor50@gmail.com>
2021-04-23 16:28:26 +0100

    lots of styling

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,26 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   backgroundStyles: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    backgroundColor: 'white',
-    color: 'white',
-    position: 'relative',
+    justifyContent: "center",
+    alignItems: "flex-start",
+    backgroundColor: "white",
+    color: "white",
+    position: "relative",
   },
   titles: {
     fontSize: 10,
-    color: 'white',
-    fontFamily: 'Nunito',
+    color: "white",
+    fontFamily: "Nunito",
   },
 
   logo: {
     flex: 1,
     height: 50,
     width: 90,
-    alignSelf: 'center',
-    position: 'absolute',
+    alignSelf: "center",
+    position: "absolute",
     top: 0,
     left: 0,
     zIndex: 1,
@@ -28,26 +28,26 @@ export default StyleSheet.create({
   button: {
     height: 47,
     borderRadius: 5,
-    backgroundColor: '#788eec',
+    backgroundColor: "#788eec",
     width: 80,
-    justifyContent: 'center',
+    justifyContent: "center",
     margin: 2,
   },
 
   buttonDisabled: {
     height: 47,
     borderRadius: 5,
-    backgroundColor: '#567eed',
+    backgroundColor: "#567eed",
     width: 80,
-    justifyContent: 'center',
+    justifyContent: "center",
     margin: 2,
   },
 
   input: {
     height: 48,
     borderRadius: 5,
-    overflow: 'hidden',
-    backgroundColor: 'white',
+    overflow: "hidden",
+    backgroundColor: "white",
     marginTop: 10,
     marginBottom: 10,
     marginLeft: 30,
@@ -55,12 +55,12 @@ export default StyleSheet.create({
     paddingLeft: 16,
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
   paragraphText: {
     fontSize: 10,
-    color: 'black',
-    fontFamily: 'Nunito',
+    color: "black",
+    fontFamily: "Lato_400Regular",
   },
 });
