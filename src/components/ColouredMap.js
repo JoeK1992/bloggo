75c52aa187da7552e1e42a3ec0e2198707090a6f
Gary OConnor <garyoconnor50@gmail.com>
2021-04-27 16:05:32 +0100

    map styled, stats styled, navbar logo changed

--- src/components/ColouredMap.js
+++ src/components/ColouredMap.js
@@ -1,35 +1,116 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import MapView, { Polygon } from 'react-native-maps';
-import { fetchCoordinates } from './utils/GeoJsonUtils';
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import MapView, { Polygon } from "react-native-maps";
+import { fetchCoordinates } from "./utils/GeoJsonUtils";
+
+mapstyle = [
+  {
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.neighborhood",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.man_made",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#ffea05",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+];
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#52b69a",
+    borderWidth: 7,
   },
   map: {
-    width: 500,
-    height: 500,
-    position: 'relative',
+    width: 400,
+    height: 450,
+    position: "relative",
   },
 });
 
-const example = ['Indonesia', 'Canada', 'France', 'San Marino', 'Finland'];
-
 class ColouredMap extends Component {
   render() {
+    const { countries } = this.props;
+
     return (
       <View style={styles.container}>
-        <MapView style={styles.map}>
-          {example.map((country) => {
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 15.1201,
+            longitude: -23.6052,
+            latitudeDelta: 180,
+            longitudeDelta: 360,
+          }}
+          customMapStyle={mapstyle}
+        >
+          {countries.map((country) => {
             return fetchCoordinates(country).map((polygon, index) => {
               return (
                 <Polygon
                   coordinates={polygon}
-                  fillColor="#0000ff"
+                  fillColor="#52b69a"
                   key={index}
                 />
               );
