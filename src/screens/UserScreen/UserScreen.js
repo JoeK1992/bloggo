e741f371ce2f60991e86bef1b94975f59c5f7e01
Gary OConnor <garyoconnor50@gmail.com>
2021-04-28 15:58:36 +0100

    userpage bugs fixed

--- src/screens/UserScreen/UserScreen.js
+++ src/screens/UserScreen/UserScreen.js
@@ -1,31 +1,31 @@
-import "firebase/auth";
-import "firebase/firestore";
-import React, { Component } from "react";
+import 'firebase/auth';
+import 'firebase/firestore';
+import React, { Component } from 'react';
 import {
   ScrollView,
   Text,
   TouchableOpacity,
   View,
   ImageBackground,
-} from "react-native";
-import AddAvatar from "../../components/AddAvatar";
-import ColouredMap from "../../components/ColouredMap";
+} from 'react-native';
+import AddAvatar from '../../components/AddAvatar';
+import ColouredMap from '../../components/ColouredMap';
 // ActivityIndicator,
-import NavBar from "../../components/NavBar";
-import ProfileHeader from "../../components/ProfileHeader";
-import firebase from "../../firebase/config";
+import NavBar from '../../components/NavBar';
+import ProfileHeader from '../../components/ProfileHeader';
+import firebase from '../../firebase/config';
 // import TripsScreen from '../TripsScreen/TripsScreen';
 // import AddAvatar from '../../components/AddAvatar';
 // import globe from '../../images/globe.png';
 // import trip from '../../images/trip.png';
 // import country from '../../images/country.jpeg';
 // import continent from '../../images/continents.jpg';
-import first from "../../images/1.jpeg";
-import second from "../../images/2.jpg";
-import third from "../../images/3.jpg";
-import fourth from "../../images/4.jpg";
-import flagBackground from "../../images/flag.jpg";
-import styles from "./styles";
+import first from '../../images/1.jpeg';
+import second from '../../images/2.jpg';
+import third from '../../images/3.jpg';
+import fourth from '../../images/4.jpg';
+import flagBackground from '../../images/flag.jpg';
+import styles from './styles';
 
 // const { height, width } = Dimensions.get('window');
 class UserScreen extends Component {
@@ -54,14 +54,14 @@ class UserScreen extends Component {
     }
 
     const db = firebase.firestore();
-    const tripsRef = db.collection("trips");
+    const tripsRef = db.collection('trips');
     tripsRef
-      .where("user", "==", currentUserUID)
-      .where("summary", "!=", false)
+      .where('user', '==', currentUserUID)
+      .where('summary', '!=', false)
       .get()
       .then((snapshot) => {
         if (snapshot.empty) {
-          console.log("No matching documents.");
+          console.log('No matching documents.');
         } else {
           const tripUidsArr = [];
           let trips = 0;
@@ -75,12 +75,12 @@ class UserScreen extends Component {
           const { tripUids } = this.state;
           tripUids.forEach((tripUid) => {
             const destinationsRef = db
-              .collection("trips")
+              .collection('trips')
               .doc(tripUid)
-              .collection("destinations");
+              .collection('destinations');
             destinationsRef.get().then((snapshot) => {
               if (snapshot.empty) {
-                console.log("No matching documents.");
+                console.log('No matching documents.');
               } else {
                 const continents = [];
                 const countries = [];
@@ -124,20 +124,22 @@ class UserScreen extends Component {
     } else {
       usersOwnProfile = false;
       currentUserUID = firebase.auth().currentUser.uid;
-      page = "My Profile";
+      page = 'My Profile';
     }
-    const { trips, continents, countries, flags } = this.state;
+    const {
+      trips, continents, countries, flags,
+    } = this.state;
 
     const globePercentage = Math.round((countries.length / 195) * 100);
 
     const { navigation } = this.props;
 
     return (
-      // <View>
+    // <View>
 
-      /* {loading ?  <ActivityIndicator /> :  */
+    /* {loading ?  <ActivityIndicator /> :  */
 
-      <View style={{ flex: 1, backgroundColor: "#1E6091" }}>
+      <View style={{ flex: 1, backgroundColor: '#1E6091' }}>
         <ScrollView style={styles.userScreen}>
           <ProfileHeader userUID={currentUserUID} />
           {/* <View style={styles.mapContainer} /> */}
@@ -147,10 +149,10 @@ class UserScreen extends Component {
             <ColouredMap countries={countries} />
           </View>
 
-          {page === "My Profile" && (
+          {page === 'My Profile' && (
             <View style={styles.btnContainer}>
               <TouchableOpacity
-                onPress={() => navigation.navigate("Trips", { page: "" })}
+                onPress={() => navigation.navigate('Trips', { page: '' })}
                 style={styles.btn}
               >
                 <Text style={styles.text}>My Trips</Text>
@@ -169,7 +171,7 @@ class UserScreen extends Component {
               >
                 <Text style={styles.statsCardText}>
                   {continents.length === 1
-                    ? "1 Continent"
+                    ? '1 Continent'
                     : `${continents.length} Continents`}
                 </Text>
               </ImageBackground>
@@ -181,7 +183,7 @@ class UserScreen extends Component {
               >
                 <Text style={styles.statsCardText}>
                   {countries.length === 1
-                    ? "1 Country"
+                    ? '1 Country'
                     : `${countries.length} Countries`}
                 </Text>
               </ImageBackground>
@@ -192,7 +194,7 @@ class UserScreen extends Component {
                 style={styles.statsCard}
               >
                 <Text style={styles.statsCardText}>
-                  {trips === 1 ? "1 Trip" : `${trips} Trips`}
+                  {trips === 1 ? '1 Trip' : `${trips} Trips`}
                 </Text>
               </ImageBackground>
 
